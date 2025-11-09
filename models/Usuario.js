c2a2920331d25f030e6a7e54e21c3ef7e277abfe
stiven <stiven@gmail.com>
2025-11-08 21:07:45 -0500

    FEAT: :zap: CREATING MODEL USER

---
+++ models/Usuario.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const usuarioSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  correo: { type: String, required: true, unique: true },
+  edad: { type: Number, required: true }
+});
+
+export const Usuario = mongoose.model("Usuario", usuarioSchema);
