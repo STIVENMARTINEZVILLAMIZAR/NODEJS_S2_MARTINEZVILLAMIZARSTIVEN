119de6e7a38c2ead7cffab11ec76a9dcc4f5e449
stiven <stiven@gmail.com>
2025-11-08 21:16:41 -0500

    FEAT: :zap:FILE ROUTES

---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { UsuarioController } from "../controllers/usuarioController.js";
+
+const router = express.Router();
+
+router.post("/", UsuarioController.crear);
+router.get("/", UsuarioController.listar);
+router.put("/:id", UsuarioController.actualizar);
+router.delete("/:id", UsuarioController.eliminar);
+
+export default router;
